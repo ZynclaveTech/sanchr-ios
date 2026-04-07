b983d2c13e98526c8ac7cabd289be2ed8fe1854f
Sooraj Pandey <surajpandey0016@gmail.com>
2026-04-07 21:12:49 +0530

    chore: generate build artifacts for SanchrShared framework

---
+++ build/Sanchr.build/Debug-iphoneos/SanchrShared.build/Objects-normal/arm64/SanchrShared_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/soorajpandey/Projects/zynclave/sanchr/ios/Sanchr-iOS/build/Debug-iphoneos/SanchrShared.framework/Modules/module.modulemap \
+  /Users/soorajpandey/Projects/zynclave/sanchr/ios/Sanchr-iOS/build/Sanchr.build/Debug-iphoneos/SanchrShared.build/DerivedSources/SanchrShared_vers.c
